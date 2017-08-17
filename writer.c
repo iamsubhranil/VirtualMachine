871ba23bb30788255295cac59d0b2e0242071cc6
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2017-08-17 19:50:23 +0530

    Added optional bitwise explanation

--- writer.c
+++ writer.c
@@ -24,12 +24,16 @@ static void writeFooter(FILE *fp){
 static void writeOperand(Operand op, FILE *fp){
 	fwrite(&(op.mode), sizeof(uint8_t), 1, fp);
 	Data d = op.data;
+	//printf("\t%8x", op.mode);
 	switch(op.mode){
 		case IMMEDIATE: fwrite(&(d.imv), sizeof(uint32_t), 1, fp);
+				//printf("\t%4x", d.imv);
 				break;
 		case REGISTER: fwrite(&(d.rega), sizeof(uint8_t), 1, fp);
+			       //printf("\t%4x", d.rega);
 			       break;
 		case DIRECT: fwrite(&(d.mema), sizeof(uint16_t), 1, fp);
+			     //printf("\t%4x", d.mema);
 			     break;
 		case VARIABLE: break;
 	}
@@ -58,6 +62,8 @@ void writeBinary(Instruction ins[], uint16_t length, Machine *m, char *filename)
 	}
 	uint16_t i = 0;
 	writeHeader(fp, length);
+	//printf("\nOpcode\tFormat\tAddressingMode1\tValue1\tAddressingMode2\tValue2\tAddressingMode3\tValue3");
+	//printf("\n======\t======\t===============\t======\t===============\t======\t===============\t======");
 	while(i<length){
 		Instruction in = ins[i];
 		switch(in.format){
@@ -75,7 +81,9 @@ void writeBinary(Instruction ins[], uint16_t length, Machine *m, char *filename)
 					   convertVariableToDirect(&in.operands.threa.op3, m);
 					   break;
 		}
+		//printf("\n%5x", in.opcode);
 		fwrite(&(in.opcode), sizeof(uint8_t), 1, fp);
+		//printf("\t%5x", in.format);
 		fwrite(&(in.format), sizeof(uint8_t), 1, fp);
 		writeOperands(in, fp);
 		i++;
