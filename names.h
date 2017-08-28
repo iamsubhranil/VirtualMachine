97e97126095016f225aa3ba5a637dd0032bbf783
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2017-08-28 21:34:29 +0530

    Added mod function name

--- names.h
+++ names.h
@@ -2,7 +2,7 @@
 #define NAMES_H
 
 static char *insNames[] = {"incr", "decr", "unlet", "print", "load", "store", "let", "halt", "add", "sub", "mul", "div",
-                           "setl", "jne", "jlt", "jgt", "prntl", "inpti", "inpts"};
+                           "setl", "jne", "jlt", "jgt", "prntl", "inpti", "inpts", "prompt", "prmptl"};
 static char *formatNames[] = {"zeroa", "onea", "twoa", "threa"};
 static char *modeNames[] = {"imm", "reg", "dir", "var", "ims"};
 static char *binaryFormat[] = {"FLEXIBLE", "OPTIMISED"};
