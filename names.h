e578e86d8bd9fefb2fd7388572ac50155e9d10ee
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2017-08-28 16:25:32 +0530

    Added new operation names

--- names.h
+++ names.h
@@ -2,9 +2,9 @@
 #define NAMES_H
 
 static char *insNames[] = {"incr", "decr", "unlet", "print", "load", "store", "let", "halt", "add", "sub", "mul", "div",
-                           "setl", "jne", "jlt", "jgt"};
+                           "setl", "jne", "jlt", "jgt", "prntl", "inpti", "inpts"};
 static char *formatNames[] = {"zeroa", "onea", "twoa", "threa"};
-static char *modeNames[] = {"imm", "reg", "dir", "var"};
+static char *modeNames[] = {"imm", "reg", "dir", "var", "ims"};
 static char *binaryFormat[] = {"FLEXIBLE", "OPTIMISED"};
 static char *cellNames[] = {"INSTRUCTION", "DATA"};
 
